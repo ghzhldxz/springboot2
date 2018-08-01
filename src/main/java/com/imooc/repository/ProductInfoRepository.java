5eb656f01ee1600c83a03bb29fd3400afbfdd7d3
guanhuizhenwj64144 <guanhuizhenwj64144@touna.cn>
2018-08-01 17:35:59 +0800

    商品及商品类目service层dao层开发

--- src/main/java/com/imooc/repository/ProductInfoRepository.java
+++ src/main/java/com/imooc/repository/ProductInfoRepository.java
@@ -1,6 +1,7 @@
 package com.imooc.repository;
 
 import com.imooc.dataobject.ProductInfo;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +13,5 @@ import java.util.List;
 public interface ProductInfoRepository extends JpaRepository<ProductInfo, String> {
 
     List<ProductInfo> findByProductStatus(Integer productStatus);
+
 }
